14ee1e05084837f1cec502493b341d9861877861
michele <michele.damico@gmail.com>
2017-08-10 08:34:40 +0200

    Initial Check-in: lazy property both thread safe and not

---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+#![feature(optin_builtin_traits)]
+
+#[macro_use(debug_unreachable)]
+extern crate debug_unreachable;
+
+use std::cell::UnsafeCell;
+use std::sync::Mutex;
+
+pub struct Lazy<'a, P>
+{
+    field: UnsafeCell<Option<P>>,
+    producer: Box<Fn() -> P + 'a>
+}
+
+impl<'a, P> Lazy<'a, P>
+{
+    pub fn new<F: Fn() -> P + 'a>(f: F) -> Self
+    {
+        Lazy { field: UnsafeCell::new(None), producer: Box::new(f) }
+    }
+
+    pub fn get(&self) -> &P {
+        unsafe {
+            let inner = &mut *self.field.get();
+            if inner.is_none() {
+                *inner = Some((*self.producer)());
+            }
+            match *inner {
+                Some(ref v) => v,
+                None => debug_unreachable!()
+            }
+        }
+    }
+}
+
+pub struct LazyParam<'a, P>
+{
+    pub lazy: Lazy<'a, P>
+}
+
+impl<'a, P> LazyParam<'a, P>
+{
+    pub fn new<F: Fn() -> P + 'a>(f: F) -> Self {
+        LazyParam { lazy: Lazy::new(f) }
+    }
+}
+
+pub struct LazyThreadSafe<'a, P>
+{
+    field: Mutex<Option<P>>,
+    producer: Box<Fn() -> P + 'a + Send + Sync>
+}
+
+impl<'a, P> LazyThreadSafe<'a, P>
+{
+    pub fn new<F: Fn() -> P + 'a + Send + Sync>(f: F) -> Self
+    {
+        LazyThreadSafe { field: Mutex::new(None), producer: Box::new(f) }
+    }
+
+    pub fn get(&self) -> &P {
+        let mut inner = self.field.lock().unwrap();
+        if inner.is_none() {
+            *inner = Some((*self.producer)());
+        }
+        unsafe {
+            match *inner {
+                Some(ref v) => &*(v as *const P),
+                None => debug_unreachable!()
+            }
+        }
+    }
+}
+
+pub struct LazyThreadSafeParam<'a, P>
+{
+    pub lazy: LazyThreadSafe<'a, P>
+}
+
+impl<'a, P> LazyThreadSafeParam<'a, P>
+{
+    pub fn new<F: Fn() -> P + 'a + Send + Sync>(f: F) -> Self {
+        LazyThreadSafeParam { lazy: LazyThreadSafe::new(f) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod lazy {
+        use super::*;
+        fn param<'a, P,  F: Fn() -> P + 'a>(f: F) -> LazyParam<'a, P> {
+            LazyParam::new(f)
+        }
+
+        mod contract;
+
+    }
+
+    mod lazy_thread_safe {
+        use super::*;
+
+        fn param<'a, P, F: Fn() -> P + 'a + Send + Sync>(f: F) -> LazyThreadSafeParam<'a, P> {
+            LazyThreadSafeParam::new(f)
+        }
+
+        mod contract;
+
+        #[test]
+        fn multiple_threads_can_access_to_the_same_property() {
+            use std::thread::spawn;
+
+            let s = std::sync::Arc::new(param(|| 42));
+
+            let handles = (0..10).map(|_| {
+                let ss = s.clone();
+                spawn(move || assert_eq!(&42, ss.lazy.get()))
+            }
+            ).collect::<Vec<_>>();
+
+            for h in handles {
+                h.join().unwrap()
+            }
+        }
+    }
+}
---
+++ src/tests/lazy/contract.rs
@@ -0,0 +1,28 @@
+use super::*;
+
+#[test]
+fn should_return_42() {
+    let s = param(|| 42);
+
+    assert_eq!(&42, s.lazy.get());
+}
+
+#[test]
+fn should_return_42_also_after_changed_backing_field_value() {
+    #[allow(non_upper_case_globals)]
+    static mut seed: i32 = 42;
+
+    let s = LazyParam::new(|| unsafe { seed });
+
+    assert_eq!(&42, s.lazy.get());
+
+    unsafe { seed = 43 };
+    assert_eq!(&42, s.lazy.get());
+}
+
+#[test]
+fn should_work_with_string_too() {
+    let p = param(|| "string slice");
+
+    assert_eq!(&"string slice", p.lazy.get());
+}
